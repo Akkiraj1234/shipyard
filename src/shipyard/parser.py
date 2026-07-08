c7be9a23b4e1120212f858a0e67883f4e518da05
unknown <akhandraj402@gmail.com>
2026-07-08 18:54:04 +0530

    idk

--- src/shipyard/parser.py
+++ src/shipyard/parser.py
@@ -1,20 +1,28 @@
 from pathlib import Path
+from enum import IntEnum
 import sys
 
-RoadmapCommand = None
-InitCommand = None
-DoctorCommand = None
+class TokenType(IntEnum):
+    word = 0
+    option = 1
+    flag = 3
 
 
-registry = {
-    "roadmap": RoadmapCommand,
-    "init": InitCommand,
-    "doctor": DoctorCommand,
-}
-
-
-
-
-def get_args():
-    args = sys.argv
-    return args
+def get_args_token():
+    args = sys.argv[1:]
+    token = []
+    i = 0
+    
+    while i < len(args):
+        if args[i].startswith("-") or args[i].startswith("--"):
+            if i < len(args)-1 and not args[i+1].startswith("-"):
+                token.append({"type:": TokenType.option, "key": args[i], "value": args[i+1]})
+                i += 2
+            else:
+                token.append({"type": TokenType.flag, "value": args[i]})
+                i += 1
+        
+        token.append({"type": TokenType.word, "value": args[i]})
+        i += 1
+    
+    return token
